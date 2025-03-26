fdb3d197a31f8eb73f89004f6208f5315d16a96f
Ali-Ghasemi-Tech <thelastblade30@gmail.com>
2025-03-26 19:08:30 +0330

    remove serializers(not needed) , update requirements

--- djangoProject/API/serializer.py
+++ djangoProject/API/serializer.py
@@ -1,19 +1,4 @@
 from rest_framework import serializers
 
 from .models import Hospitals
-
-
-class ItemSerializer(serializers.ModelSerializer):
-    insurance_name = serializers.CharField(max_length = 200)
-    
-    class Meta:
-
-        model = Hospitals
-
-        fields = ('insurance' , 'name')
-        
-class NeshanSerializer(serializers.Serializer):
-    hospital_name = serializers.CharField()
-    lat = serializers.DecimalField()
-    lan = serializers.DecimalField()
     
