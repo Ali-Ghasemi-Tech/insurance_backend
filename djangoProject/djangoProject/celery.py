2244c4f5264927be280ec4891af8699a0d1b3f8b
Ali-Ghasemi-Tech <thelastblade30@gmail.com>
2025-03-28 14:49:22 +0330

    fix circular import in celery.py

--- djangoProject/djangoProject/celery.py
+++ djangoProject/djangoProject/celery.py
@@ -1,6 +1,6 @@
 # myproject/celery.py
 import os
-from djangoProject.celery import Celery
+from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings')
 app = Celery('djangoProject')
 app.config_from_object('django.conf:settings', namespace='CELERY')
